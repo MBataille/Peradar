f26490e22e78f93a3207e35a6e9338afca757cfe
Martin Bataille <martinbataille@gmail.com>
2017-12-12 18:12:08 -0300

    Version 0.1.0
    * Se agrego la funcionalidad config
    * Se arreglaron errores con musica
    * Se arreglaron errores con el tiempo

--- config.py
+++ config.py
@@ -1,24 +1,23 @@
 import os, sys
 
 file_name = 'config.ini'
-params = dict()
+
 keys = ['USERNAME', 'PASSWORD', 'RAMOS', 'CONTROL', 'PREGUNTAS', \
 'R1', 'R2', 'MUSICA', 'START', 'MENSAJES_CADA']
 doc  = {
-	'USERNAME' : 'Ingresa tu usuario de u pasaporte. Tus datos se utilizaran UNICAMENTE para ingresar a reclamos.dim',
-	'PASSWORD' : 'Ingresa tu pass de u pasaporte. Tus datos se utilizaran UNICAMENTE para ingresar a reclamos.dim',
-	'CONTROL' : 'Ingresa el codigo del control o examen que quieres ver, \'C1\' si quieres ver el primer control, \'EX\' si quieres ver el examen.',
-	'PREGUNTAS' : 'Ingresa la cantidad de preguntas que tiene el control o examen que quieres ver.',
-	'RAMOS' : 'Ingresa tus ramos, ingresa 0 para terminar',
-	'R2' : 'OPCIONAL: Ingresa la nota por ramo que necesitas en el examen para ir a examen de segunda.\
+	'USERNAME' : 'Usuario de u pasaporte.',
+	'PASSWORD' : 'Pass de u pasaporte.',
+	'CONTROL' : 'Codigo del control o examen (\'C1\', \'EX\', etc)',
+	'PREGUNTAS' : 'Cantidad de preguntas que tiene el control o examen.',
+	'RAMOS' : 'Ramos, ingresa 0 para terminar',
+	'R2' : 'OPCIONAL: INota por ramo que necesitas en el examen para ir a examen de segunda.\
 	Si no aplica, dejalo en cero.',
-	'R1' : 'Ingresa la nota por ramo que necesitas para pasar el ramo (sin tener que dar el examen de segunda).',
+	'R1' : 'Nota por ramo que necesitas para pasar el ramo (sin tener que dar el examen de segunda).',
 	'MUSICA' : 'Ingresa las canciones que se reproduciran',
 	'START' : 'Ingresa el segundo en el que quieres que empiece cada cancion (asi te saltas la intro).',
-	'MENSAJES_CADA' : 'Ingresa cada cuanto tiempo quieres que el programa imprima \'Aun no hay notas\'.	Lo recomendado es 300 (es decir, 5 mins)'	
+	'MENSAJES_CADA' : 'Cada cuanto tiempo quieres que el programa imprima? \'Aun no hay notas\'.	Lo recomendado es 300 (es decir, 5 mins)'	
 }
-for k in keys:
-	params[k] = -1
+
 class ConfigFileError(Exception):
 	pass
 
@@ -44,50 +43,66 @@ def printError(msg):
 	print msg
 	print "##########################################"
 
-def readConfig(params):
-	for key in keys:
-		if key == 'PREGUNTAS':
-			try:
-				params[key] = int(params[key])
-			except ValueError:
-				raise ConfigFileError("Numero de preguntas no entero. Se sugiere borrar el archivo")
-		elif key == 'RAMOS':
-			params[key] = params[key].split(',')
-		elif key == 'R1' or key == 'R2':
-			if len(params[key].split(',')) != len(params['RAMOS']):
-				raise ConfigFileError("Cantidad de ramos incosistente. Se sugiere borrar el archivo")
-			params[key] = params[key].split(',')
-			for i in range(len(params[key])):
-				try:
-					params[key][i] = int(params[key][i])
-				except ValueError:
-					raise ConfigFileError("R1 o R2 no es entero. Se suguiere borrar el archivo")
-		elif key == 'MUSICA':
-			l = params[key].split(':')
-			if len(l) != len(params['RAMOS']):
-				raise ConfigFileError("Dimensiones de MUSICA inconsistente. Se sugiere borrar el archivo")
-			songs = [s.split(',') for s in l]
-			params[key] = songs
-		elif key == 'START':
-			l = params[key].split(':')
-			if len(l) != len(params['RAMOS']):
-				raise ConfigFileError("Dimensiones de START inconsistente. Se sugiere borrar el archivo")
-			start = [s.split(',') for s in l]
+def isConfigFile():
+	return os.path.isfile(file_name)
 
-			for i in range(len(start)):
-				for j in range(len(start[i])):
+def readConfig():
+	params = dict()
+	for k in keys:
+		params[k] = -1
+	with open(file_name, 'r') as f:
+		for line in f:
+			key, val = line.split('=')
+			val = val.split('\n')[0]
+			if params[key] != -1:
+				raise ConfigFileError("Parametro desconocido. Se sugiere borrar {}".format(file_name))
+			params[key] = val
+		for key in keys:
+			if key == 'PREGUNTAS':
+				try:
+					params[key] = int(params[key])
+				except ValueError:
+					raise ConfigFileError("Numero de preguntas no entero. Se sugiere borrar el archivo")
+			elif key == 'RAMOS':
+				params[key] = params[key].split(',')
+			elif key == 'R1' or key == 'R2':
+				if len(params[key].split(',')) != len(params['RAMOS']):
+					raise ConfigFileError("Cantidad de ramos incosistente. Se sugiere borrar el archivo")
+				params[key] = params[key].split(',')
+				for i in range(len(params[key])):
 					try:
-						start[i][j] = float(start[i][j])
+						params[key][i] = int(params[key][i])
 					except ValueError:
-						raise ConfigFileError("Tiempos de START no son numeros. Se sugiere borrar el archivo")
-			params[key] = start
-		elif key == 'MENSAJES_CADA':
-			try:
-				params[key] = int(params[key])
-			except ValueError:
-				raise ConfigFileError("MENSAJES_CADA no es entero. Se sugiere borrar el archivo")
-	return params
-print 'Bienvenido al sistema de configuracion!'
+						raise ConfigFileError("R1 o R2 no es entero. Se suguiere borrar el archivo")
+			elif key == 'MUSICA':
+				l = params[key].split(':')
+				if len(l) != len(params['RAMOS']):
+					raise ConfigFileError("Dimensiones de MUSICA inconsistente. Se sugiere borrar el archivo")
+				songs = [s.split(',') for s in l]
+				params[key] = songs
+			elif key == 'START':
+				l = params[key].split(':')
+				if len(l) != len(params['RAMOS']):
+					raise ConfigFileError("Dimensiones de START inconsistente. Se sugiere borrar el archivo")
+				start = [s.split(',') for s in l]
+
+				for i in range(len(start)):
+					for j in range(len(start[i])):
+						try:
+							start[i][j] = float(start[i][j])
+						except ValueError:
+							raise ConfigFileError("Tiempos de START no son numeros. Se sugiere borrar el archivo")
+				params[key] = start
+			elif key == 'MENSAJES_CADA':
+				try:
+					params[key] = int(params[key])
+				except ValueError:
+					raise ConfigFileError("MENSAJES_CADA no es entero. Se sugiere borrar el archivo")
+		return params
+def writeConfig(params):
+	with open(file_name, 'w') as f:
+		for key in keys:
+			f.write(key + '=' + toConfig(params[key]) + '\n')
 
 def toConfig(sth):
 	c = ''
@@ -117,81 +132,11 @@ assert toConfig(['1', '2', '3']) == '1,2,3'
 assert toConfig([[1,2,3],[4,5,6]]) == '1,2,3:4,5,6'
 assert toConfig([['1','2','3'],['4','5','6']]) == '1,2,3:4,5,6'
 
-if os.path.isfile(file_name):
-	print 'Hemos detectado un archivo de configuracion existente'
-	r = checkValido( 'Deseas eliminarlo y configurarlo desde cero (s)\
-	 o sobreescribir (n)?')
-	if r == 'n':
-		with open(file_name, 'r') as f:
-			for line in f:
-				key, val = line.split('=')
-				val = val.split('\n')[0]
-				if params[key] != -1:
-					raise ConfigFileError("Parametro desconocido. Se sugiere borrar archivo")
-				params[key] = val
-			params = readConfig(params)
-			print "Los parametros son los siguientes:"
-			for key in keys:
-				if key == 'RAMOS':
-					for i in range(len(params[key])):
-						print 'El ramo numero {} es {}'.format(i+1, params[key][i])
-						rc = raw_input('Si quieres cambiarlo, ingresa el nuevo valor, de lo contrario, ingresa \'n\'')
-						if rc != 'n': params[key][i] = rc
-				elif key == 'R1' or key == 'R2':
-					for i in range(len(params['RAMOS'])):
-						msg = '' if key == 'R1' else 'teniendo sobre un 4 en el examen de segunda '
-						print params['RAMOS'], params[key]
-						print 'Para {} la nota para pasar el ramo es {}'.format(params['RAMOS'][i], params[key][i])
-						opts = [str(j) for j in range(71)] + ['n']
-						r12 = checkValido('Si quieres cambiar la nota, ingresa la nueva nota,\
-						 de lo contrario ingresa \'n\'', opts = opts, imprimirOpciones = False)
-						if r12 != 'n': params[key][i] = int(r12)
-				elif key == 'MUSICA':
-					for i in range(len(params['RAMOS'])):
-						msg = ['bajo un {}'.format(params['R2'][i]),
-						'entre un {} y un {}'.format(params['R2'][i], params['R1'][i]),
-						'sobre un {}'.format(params['R1'][i])]
-						for j in range(3):
-							print 'Se reproducira {} si tienes '.format(params[key][i][j]) + msg[j] + ' en {}'.format(params['RAMOS'][i])
-							rm1 = raw_input('Si quieres cambiar la cancion, ingresa el nombre (ejemplo.mp3),\
-								de lo contrario, ingresa \'n\'')
-							if rm1 != 'n': params[key][i][j] = rm1
-				elif key == 'START':
-					for i in range(len(params['MUSICA'])):
-						for j in range(len(params['MUSICA'][i])):
-							print 'Se reproducira {} desde el segundo {}'.format(params['MUSICA'][i][j],
-								params['START'][i][j])
-							rs = raw_input('Si quieres cambiarlo, ingresa el segundo desde el cual quieres comience,\
-								de lo contrario ingresa \'n\'')
-							if rs != 'n':
-								notFloat = True
-								while notFloat:
-									try:
-										params[key][i][j] = float(rm1)
-										notFloat = False
-									except ValueError: 	
-										rs = raw_input('Ingresa un numero!')
-				elif key == 'MENSAJES_CADA':
-					print key + ' = ' + toConfig(params[key])
-					rc = raw_input('Si quieres cambiarlo, ingresa el nuevo valor, de lo contrario, ingresa \'n\'')
-					if rc != 'n':
-						notInt = True
-						while notInt:
-							try:
-								params[key] = int(rc)
-								notInt = False
-							except ValueError:
-								rc = raw_input('Ingresa un entero!')
-
-				else:
-					print key + ' = ' + toConfig(params[key])
-					rg = raw_input('Si quieres cambiarlo, ingresa el nuevo valor, de lo contrario, ingresa \'n\'')
-					if rg != 'n':
-						params[key] = rg
-		print "La configuracion ha terminado! Ya puedes correr run.py, gracias por tu paciencia!"
-		sys.exit()
-
-with open(file_name, 'w') as f:
+def setConfig():
+	print 'Bienvenido al sistema de configuracion!'
+	params = dict()
+	for k in keys:
+		params[k] = -1
 	for key in keys:
 		print doc[key]
 		if key == 'R1' or key == 'R2':
@@ -246,5 +191,5 @@ with open(file_name, 'w') as f:
 						re = raw_input('Ingresa un entero!')
 			else:
 				params[key] = re
-		f.write(key + '=' + toConfig(params[key]) + '\n')
-print "La configuracion ha terminado! Ya puedes correr run.py, gracias por tu paciencia!"
+		writeConfig(params)
+	print "La configuracion ha terminado!"
